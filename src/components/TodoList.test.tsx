3e65b130982e7cce23c547b91a2206bc97f5aa7f
Jeffrey Roberts <57999176+Jeffrey-Roberts@users.noreply.github.com>
2023-08-29 20:26:35 -0500

    added tests

--- src/components/TodoList.test.tsx
+++ src/components/TodoList.test.tsx
@@ -2,6 +2,17 @@ import { render, screen } from '@testing-library/react';
 import TodoList from './TodoList';
 import TodoItem from './TodoItem';
 
+describe('render header columns', () => {
+  test.each(['Task', 'Description', 'Timestamp'])(
+    'should render header',
+    (label) => {
+      render(<TodoList todoItemList={[]} />);
+      const result = screen.getByText(label);
+      expect(result).toBeInTheDocument();
+    }
+  );
+});
+
 test('should render todo items', () => {
   render(<TodoList todoItemList={[new TodoItem('Name', 'Description')]} />);
   expect(screen.getByLabelText('todo list')).toBeInTheDocument();
